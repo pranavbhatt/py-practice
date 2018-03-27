67667efa319b2e6cef0216e7cbe9fa588c3c5c46
Pranav Bhatt <bhattpranavv@gmail.com>
2018-03-26 21:27:03 -0700

    Testing authenticated user

--- tdd_example/twit/tests/test_views.py
+++ tdd_example/twit/tests/test_views.py
@@ -1,8 +1,26 @@
+import pytest
 from django.test import RequestFactory
+from django.contrib.auth.models import AnonymousUser
+from mixer.backend.django import mixer
 from .. import views
+pytestmark = pytest.mark.django_db
 
 class TestHomeView:
     def test_anonymous(self):
         req = RequestFactory().get('/')
         resp = views.HomeView.as_view()(req)
         assert resp.status_code == 200, 'Should be callable by anyone'
+
+class TestAdminView:
+    def test_anonymous(self):
+        req = RequestFactory().get('/')
+        req.user = AnonymousUser()
+        resp = views.AdminView.as_view()(req)
+        assert 'login' in resp.url
+    
+    def test_authenticated_user(self):
+        user = mixer.blend('auth.User', is_superuser=True)
+        req = RequestFactory().get('/')
+        req.user = user
+        resp = views.AdminView.as_view()(req)
+        assert resp.status_code == 200, 'Authenticated user can access'
--- tdd_example/twit/views.py
+++ tdd_example/twit/views.py
@@ -1,4 +1,14 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 class HomeView(TemplateView):
     template_name = 'twit/home.html'
+
+
+class AdminView(TemplateView):
+    template_name = 'twit/admin.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(AdminView, self).dispatch(request,*args, **kwargs)
